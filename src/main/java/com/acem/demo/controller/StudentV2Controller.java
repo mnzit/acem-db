189528c36c34d7199eca30b0823cf9ed1fe2810d
mnzit <mnzitshakya@gmail.com>
2022-11-17 17:30:42 +0545

    Validator for JakartaEE

--- src/main/java/com/acem/demo/controller/StudentV2Controller.java
+++ src/main/java/com/acem/demo/controller/StudentV2Controller.java
@@ -4,6 +4,7 @@ import com.acem.demo.request.StudentSaveRequest;
 import com.acem.demo.request.mapper.StudentMapperUtil;
 import com.acem.demo.service.StudentService;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -28,7 +29,7 @@ public class StudentV2Controller {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response create(StudentSaveRequest studentSaveRequest) {
+    public Response create(@Valid StudentSaveRequest studentSaveRequest) {
         com.acem.demo.response.Response responseBody = studentService.save(StudentMapperUtil.mapStudent(studentSaveRequest));
 
         return Response
